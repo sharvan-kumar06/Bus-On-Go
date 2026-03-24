270b64c6c2f601dc82dc046c5bac9d951c3727fa
sharvan-kumar06 <sharvankumar10a@gmail.com>
2026-03-24 09:11:43 +0530

    Changed webhooks

--- src/lib/webhooks.ts
+++ src/lib/webhooks.ts
@@ -1,9 +1,9 @@
 // Centralized webhook URLs - import these where needed
 export const OTP_WEBHOOK_URL =
-  "https://n8n.sharvandev.xyz/webhook/otp-auth";
+  "https://eo53ykrll54b5ss.m.pipedream.net";
 
 export const BOOKING_WEBHOOK_URL =
-  "https://n8n.sharvandev.xyz/webhook/booking-completed";
+  "https://eoe300m2w0b78nx.m.pipedream.net";
 
 export const CANCEL_BOOKING_WEBHOOK_URL =
-  "https://n8n.sharvandev.xyz/webhook/booking-cancel-confirmation";
+  "https://eoomj7h4fd92kvw.m.pipedream.net";
